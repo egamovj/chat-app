816e64508c1c5ff5eedac18f526354c6120a3fcf
egamovj <egamovj90@gmail.com>
2024-05-02 12:48:53 +0500

    user list design completed

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -4,6 +4,7 @@ import "./chatList.css";
 import search from "../../../../public/search.png";
 import plus from "../../../../public/plus.png";
 import minus from "../../../../public/minus.png";
+import avatar from "../../../../public/avatar.png";
 
 const ChatList = () => {
   const [addMode, setAddMode] = useState(false);
@@ -21,6 +22,42 @@ const ChatList = () => {
           className="add"
           onClick={() => setAddMode((prev) => !prev)}
         />
+      </div>
+
+      <div className="item">
+        <img src={avatar} alt="avatar" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src={avatar} alt="avatar" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src={avatar} alt="avatar" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src={avatar} alt="avatar" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src={avatar} alt="avatar" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
       </div>
     </div>
   );
