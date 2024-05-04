9788c891f9793cc0e98ddd9e39d116fb02441c2a
egamovj <egamovj90@gmail.com>
2024-05-04 16:08:50 +0500

    create new chat

--- src/components/addUser/AddUser.jsx
+++ src/components/addUser/AddUser.jsx
@@ -1,11 +1,25 @@
 import React, { useState } from "react";
 
 import "./addUser.css";
-import { collection, getDocs, query, serverTimestamp, setDoc, where } from "firebase/firestore";
+import {
+  arrayUnion,
+  collection,
+  doc,
+  getDocs,
+  query,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import { db } from "../../lib/firebase";
+import { useUserStore } from "../../lib/userStore";
 
 const AddUser = () => {
   const [user, setUser] = useState(null);
+
+  const {currentUser} = useUserStore()
+
   const handleSearch = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
@@ -29,11 +43,32 @@ const AddUser = () => {
     const userChatsRef = collection(db, "userchats");
 
     try {
-      await setDoc(chatRef, {
-        createdAt: serverTimestamp(),
-        // messages: 
-      })
+      const newChatRef = doc(chatRef);
 
+      await setDoc(newChatRef, {
+        createdAt: serverTimestamp(),
+        messages: [],
+      });
+
+      await updateDoc(doc(userChatsRef, user.id), {
+        chats: arrayUnion({
+          chatId: newChatRef.id,
+          lastMessage: "",
+          receiverId: currentUser.id,
+          updatedAt: Date.now(),
+        }),
+      });
+
+      await updateDoc(doc(userChatsRef, currentUser.id), {
+        chats: arrayUnion({
+          chatId: newChatRef.id,
+          lastMessage: "",
+          receiverId: user.id,
+          updatedAt: Date.now(),
+        }),
+      });
+
+      console.log(newChatRef.id);
     } catch (err) {
       console.log(err);
     }
